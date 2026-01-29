b22ace310ed625fa06757b8f2aa7038c75569e70
Jadarma <dancristiancimpianu@gmail.com>
2026-01-29 23:56:35 +0200

    Cinterop with stb image.

--- build.gradle.kts
+++ build.gradle.kts
@@ -15,6 +15,16 @@ kotlin {
         linuxArm64(),
         macosArm64(),
     ).forEach { target ->
+        target.compilations.getByName("main") {
+            cinterops {
+                val stbImage by creating {
+                    val dir = project.file("src/nativeInterop/cinterop/stbImage")
+                    definitionFile.set(dir.resolve("stb_image.def"))
+                    packageName("stbImage")
+                    compilerOpts("-I/${dir}")
+                }
+            }
+        }
         target.binaries.executable {
             entryPoint = "io.github.jadarma.steggo.main"
         }
